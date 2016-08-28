fdeb387e2af0bbc680295ac40b7543c655589a3c
Rishav Agarwal <rishav.159@gmail.com>
2016-08-28 08:25:59 +0530

    Callback added

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
 var mongoose = require('mongoose');
 var assert = require('assert');
 mongoose.connect('mongodb://localhost/cet-chat');
-mongoose.Promise = require('bluebird');
+
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
 
   var userSchema = mongoose.Schema({
-    username: String,
-    password: String,
+    username: {type:String, required:true, unique:true},
+    password: {type:String, required:true},
     rooms: Array
   });
 
@@ -18,12 +18,15 @@ db.once('open', function() {
   var newUser = new user({ username: 'Sandy', password: 'fuckyou' });
   console.log(newUser.username);
 
-  var query = newUser.save();
-  assert.equal(query.exec().constructor, require('bluebird'));
+  newUser.save(function(){
+    user.find(function (err, user) {
+      if (err) return console.error(err);
+      console.log(user);
+    });
+  });
 
-  user.find(function (err, user) {
-    if (err) return console.error(err);
-    console.log(user);
-  })
+  //assert.equal(query.exec().constructor, require('bluebird'));
+
+
 
 });
