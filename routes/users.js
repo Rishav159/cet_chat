87ee57b2602f953cd1c35a9956f251c892118cf6
Rishav Agarwal <rishav.159@gmail.com>
2016-08-28 08:45:40 +0530

    Link added

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+var User = require('/user');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/signUp', function(req, res, next) {
+  var newUser = {};
+  newUser.name = req.body.name;
+  newUser.pass = req.body.pass;
+  var response = User.insert(newUser);
+  if(response.err){
+    res.send(response.msg);
+  }else{
+    res.send("Added succesfully");
+  }
 });
 
 module.exports = router;
