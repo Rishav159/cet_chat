67c0c4dab0588503b4e54efdf7d4a7f2a4963c0a
Rishav Agarwal <rishav.159@gmail.com>
2016-08-28 09:58:50 +0530

    Changes in routing

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var User = require('/user');
+//var User = require('/user');
 /* GET users listing. */
 router.post('/signUp', function(req, res, next) {
+  console.log("Sign up called");
   var newUser = {};
   newUser.name = req.body.name;
   newUser.pass = req.body.pass;
-  var response = User.insert(newUser);
-  if(response.err){
-    res.send(response.msg);
-  }else{
-    res.send("Added succesfully");
-  }
+  console.log(newUser);
+  var response = {
+    msg: "Done"
+  };
+  res.send(response);
 });
 
 module.exports = router;
