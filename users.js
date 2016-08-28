3c3936fc5f854bb6dfae118ec51bfaf877239955
Saisankar Gochhayat <saisankargochhayat@gmail.com>
2016-08-29 03:30:54 +0530

    added check password and bycrypt implemented

--- users.js
+++ users.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
 var assert = require('assert');
+var bcrypt = require('bcrypt');
+SALT_WORK_FACTOR = 10;
 mongoose.connect('mongodb://localhost/cet-chat');
 
 
@@ -13,9 +15,22 @@ db.once('open', function() {
     rooms: Array
   });
 
-  var user = mongoose.model('user', userSchema);
 
-  var insert = function(newUser){
+  userSchema.methods.insert = function(newUser){
+    //generate salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+       if (err) return next(err);
+
+       // hash the password using our new salt
+          bcrypt.hash(newUser.pass, salt, function(err, hash) {
+              if (err) return next(err);
+
+              // override the cleartext password with the hashed one
+              newUser.pass = hash;
+              next();
+          });
+    });
+
     var newUser = new user({ username: newUser.name, password: newUser.pass });
     console.log(newUser.username);
     newUser.save(function (err, user) {
@@ -26,8 +41,27 @@ db.once('open', function() {
     });
   }
 
+  userSchema.methods.checkUser = function(inputUser){
+    var pass; var chkres={};
+    chkres.err=false;
+    user.findOne({name: inputUser.name},function(err,user){
+      pass=user.pass;
+      bcrypt.compare(inputUser.pass, pass, function(err, isMatch) {
+        if (err)
+          chkres.err=true;
+          else {
+            chkres.isMatch=isMatch;
+          }
+
+        //cb(null, isMatch);
+      });
+      return chkres;
+    });
+
+  }
+
+var user = mongoose.model('user', userSchema);
+
 });
 
-module.exports = {
-  insert:insert
-}
+module.exports = mongoose.model('User', userSchema);
