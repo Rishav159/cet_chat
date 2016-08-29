fc17c8941757ab3f4ca7476fca7d15cc89f95f3c
Saisankar Gochhayat <saisankargochhayat@gmail.com>
2016-08-29 19:53:24 +0530

    room module export

--- room.js
+++ room.js
@@ -1,26 +1,12 @@
 var mongoose = require('mongoose');
-var assert = require('assert');
 mongoose.connect('mongodb://localhost/cet-chat');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+
   var roomSchema = mongoose.Schema({
     name: {type:String, required:true, unique:true},
     createdBy: {type:String, required:true},
     members: Array
   });
-roomSchema.methods.insert = function(){
-  var newRoom= new room({name : this.name, createdBy: this.id , member: [id]});
-  newRoom.save(function (err, user) {
-    var res = {};
-    if (err) res.error = true;
-    else res.error=false;
-        return res;
-  });
-
-
-
-
-
-}
+  module.exports = mongoose.model('Room', roomSchema);
