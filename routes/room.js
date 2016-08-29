3700fc4dfb13cd3e5569589a670524c432b79dd2
Rishav Agarwal <rishav.159@gmail.com>
2016-08-29 08:42:58 +0530

    Sai ka work added

--- routes/room.js
+++ routes/room.js
@@ -5,12 +5,20 @@ var User = require('../models/room');
 router.post('/createRoom', function(req,res,next){
   console.log("Create room called");
   var createRoom = new Room({
-    name:req.body.name;
-    createdBy:req.session.user.id;
-    
-
+    name:req.body.name,
+    createdBy:req.session.user.id,
+    members:[req.session.user.id]
   })
-
-
-
-})
+  createRoom.save(function (err, user) {
+      var msg;
+      if (err){
+        console.log(err);
+        res.status = 502;
+        msg=err;
+      }else{
+        res.status = 200;
+        msg="Done !";
+      }
+      res.send(msg);
+    });
+});
