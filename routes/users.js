febf60d2d3036c60cd02101aa906c98c99fd838c
Rishav Agarwal <rishav.159@gmail.com>
2016-08-29 07:37:44 +0530

    Insert function working

--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,19 @@ var User = require('../users');
 /* GET users listing. */
 router.post('/signUp', function(req, res, next) {
   console.log("Sign up called");
-  var newUser = new User({name:req.body.name , password:req.body.pass});
-  console.log(newUser);
-  newUser.insert(function(response){
-  res.send(response);
-  });
+  var newUser = new User({username:req.body.name , password:req.body.pass});
+  newUser.save(function (err, user) {
+      var msg;
+      if (err){
+        console.log(err);
+        res.status = 502;
+        msg=err;
+      }else{
+        res.status = 200;
+        msg="Done !";
+      }
+      res.send(msg);
+    });
 });
 
 module.exports = router;
--- users.js
+++ users.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var assert = require('assert');
 var bcrypt = require('bcrypt');
 SALT_WORK_FACTOR = 10;
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/cet-chat');
 
 
@@ -13,30 +14,26 @@ db.on('error', console.error.bind(console, 'connection error:'));
     password: {type:String, required:true},
     rooms: Array
   });
+  userSchema.pre('save', function(next) {
+      var user = this;
 
+      // only hash the password if it has been modified (or is new)
+      if (!user.isModified('password')) return next();
 
-  userSchema.methods.insert = function(){
-    //generate salt
-    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-       if (err) return next(err);
+      // generate a salt
+      bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+          if (err) return next(err);
 
-       // hash the password using our new salt
-          bcrypt.hash(this.password, salt, function(err, hash) {
-              if (err) console.log("There is bcrypt error");
+          // hash the password along with our new salt
+          bcrypt.hash(user.password, salt, function(err, hash) {
+              if (err) return next(err);
 
               // override the cleartext password with the hashed one
-              newUser.password = hash;
+              user.password = hash;
+              next();
           });
-    });
-  this.save(function (err, user) {
-      var res = {};
-      if (err) res.error = true;
-      else res.error=false;
-      return res;
-    });
-
-  }
-
+      });
+  });
   userSchema.methods.checkUser = function(inputUser){
     var pass; var chkres={};
     chkres.err=false;
